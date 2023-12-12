358c276b2ba07510bdbe50f218a4bfd4481d4b3e
Knight Trennéus <knighttrenneus@hotmail.com>
2023-12-12 20:06:09 +0100

    done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { useReducer, useState } from "react";
+import { Pressable, StyleSheet, Text, Vibration, View } from "react-native";
 
 export default function App() {
   const [isPressed, setPressed] = useState<boolean>(false);
   const [startTime, setStartTime] = useState<string | null>();
   const [endTime, setEndTime] = useState<string | null>();
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
   function buttonPressed() {
     if (!isPressed) {
@@ -18,24 +19,41 @@ export default function App() {
     }
   }
 
-  function timeDiff() {
+  function showTime() {
     if (endTime != null && startTime != null) {
       const diffMs = +new Date(endTime) - +new Date(startTime);
-
       const ms = (diffMs / 1000).toFixed(3);
-
-      return `${ms} s`;
+      return `${ms} cl`;
+    } else if (startTime != null) {
+      const curTime = new Date().toISOString();
+      const diffMs = +new Date(curTime) - +new Date(startTime);
+      const ms = (diffMs / 1000).toFixed(3);
+      setTimeout(() => {
+        forceUpdate();
+      }, 10);
+      return `${ms} cl`;
     } else {
-      return null;
+      return `0 cl`;
     }
   }
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => buttonPressed()}>
-        {isPressed ? <Text>I'm pressed</Text> : <Text>I'm not pressed</Text>}
+      <Pressable
+        style={(state) => [
+          styles.button,
+          state.pressed && { backgroundColor: "tomato" },
+        ]}
+        onPressIn={() => buttonPressed()}
+        onPressOut={() => buttonPressed()}
+      >
+        {isPressed ? (
+          <Text style={styles.buttonText}>I'm pressed</Text>
+        ) : (
+          <Text style={styles.buttonText}>I'm not pressed</Text>
+        )}
       </Pressable>
-      <Text>{timeDiff() ?? "NO TIME NO TIME"}</Text>
+      <Text>{showTime()}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -47,5 +65,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  buttonText: {
+    fontWeight: "700",
+    color: "white",
   },
 });
